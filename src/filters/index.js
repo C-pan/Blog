20b270d8109c844b773163a99def065c0a56aee8
C-pan <876846425@qq.com>
2017-04-12 01:56:10 +0800

    b

---
+++ src/filters/index.js
@@ -0,0 +1,13 @@
+// import {normalTime} from 'normalTime'
+// // export default{
+// // 	normolTime,
+// // }
+// module.exports={
+// 	normalTime
+// }
+
+import {normalTime} from './normalTime';
+
+export default{
+	normalTime
+};
